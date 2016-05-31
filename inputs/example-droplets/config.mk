d6de7a8bce1e83d439658a2fd8c8cf863930c5c5
Urbain Vaes <urbain@vaes.uk>
2016-05-31 12:05:04 +0100

    Add example input

---
+++ inputs/example-droplets/config.mk
@@ -0,0 +1,4 @@
+DIMENSION = 2               # Dimension of the problem
+GEOMETRY = square.geo       # File describing the geometry
+PROBLEM = coalescence.pde   # File describing the problem
+VIEW = view.py              # File used for post-processing
