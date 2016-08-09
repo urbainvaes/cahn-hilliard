0cb28ed95d2cea57bae5099f389c267f86587dff
Urbain Vaes <urbain@vaes.uk>
2016-08-09 10:56:20 +0100

    Update ip address for ssh

--- ssh.mk
+++ ssh.mk
@@ -6,9 +6,9 @@ all :
 run-live :
 	ssh uv113@macomp01.ma.ic.ac.uk 'cd cahn-hilliard; \
 		make clean; \
-		make remote-mesh host="urbain@155.198.212.223"; \
+		make remote-mesh host="urbain@155.198.212.221"; \
 		make run; \
-		make remote-video host="urbain@155.198.212.223";'
+		make remote-video host="urbain@155.198.212.221";'
 
 #  Run on remote machine
 host ?= localhost
