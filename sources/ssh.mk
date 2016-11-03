067bbbf492528f126c5f0c478b6a07d6476f69eb
Urbain Vaes <urbain@vaes.uk>
2016-11-03 14:00:54 +0000

    Move variables from ssh.mk to Makefile

--- sources/ssh.mk
+++ sources/ssh.mk
@@ -1,8 +1,6 @@
 ###########################
 #  Run on remote machine  #
 ###########################
-gitRoot = $(shell git rev-parse --show-toplevel)
-problem = $(shell realpath -s --relative-to $(gitRoot)/tests $(CURDIR))
 remoteHost ?= uv113@macomp01.ma.ic.ac.uk
 remoteRoot ?= /home/ma/u/uv113/cahn-hilliard
 
