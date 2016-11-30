7ca1045536af1ba747d066a391bfff653924e8de
Urbain Vaes <urbain@vaes.uk>
2016-11-30 12:04:10 +0000

    Add scripts for computation of height and advancing point

---
+++ sources/bin/extractDropletHeight.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+temp_file=$(mktemp)
+n=$(ls output/iso/contactLine* | wc -l)
+for ((i=0;i<=$n;i=i+1));do 
+sed '$ d' "output/iso/contactLine"$i".dat" > ${temp_file}
+python << END
+import numpy as np
+x=np.genfromtxt(r''+"${temp_file}")
+print(min(x[:,1]))
+END
+done
