bc3fe73090805db7b43e318e4208b146f21a14ad
Urbain Vaes <urbain@vaes.uk>
2017-10-24 22:29:51 +0100

    Add python script for plots in 2D

--- sources/bin/mesh.py
+++ sources/bin/mesh.py
@@ -1,5 +1,6 @@
 import gmshType
 import time
+import matplotlib.tri as tri
 
 class mesh() :
   class element():
@@ -25,15 +26,16 @@ class mesh() :
       self.dimension = dim
       self.physicals = physicals
       self.elements = []
-      
+
   def __init__(self, filename = None) :
     self.vertices = []
+    self.data = []
     self.physicals = [{}, {}, {}, {}]
     self.entities = []
     self.maxeid = 0
     self.useFormat3 = False
     if filename :
-      self.read(filename)
+      self.read_mesh(filename)
 
   def write(self, filename, format3=None) :
     output = open(filename, "w")
@@ -81,7 +83,7 @@ class mesh() :
           output.write("%i %i %i %i %i %s %s\n" % (e.tag, e.etype.tag, ntag, ptag, entity.tag, spart, " ".join(repr(v[3]) for v in e.vertices)))
       output.write("$EndElements\n")
 
-  def read(self, filename):
+  def read_mesh(self, filename):
     fin = open(filename, "r");
     l = fin.readline()
     vmap = {}
@@ -93,7 +95,7 @@ class mesh() :
         if float(l[0]) == 3.:
           self.useFormat3 = True
         elif int(float(l[0])) == 2 :
-          self.useFormat3 = False 
+          self.useFormat3 = False
         else :
           print("error : cannot read mesh format " + l[0])
         l = fin.readline()
@@ -139,10 +141,88 @@ class mesh() :
             entity = mesh.entity(e, edim, [p])
             self.entities.append(entity)
             entitymap[(edim, e)] = entity
-          entity.elements.append(mesh.element(gmshType.Type[t], vertices, partition, j)) 
+          entity.elements.append(mesh.element(gmshType.Type[t], vertices, partition, j))
           self.maxeid = max(self.maxeid, j)
       l = fin.readline()
 
+  def read_data(self, filename):
+    fin = open(filename, "r");
+    l = fin.readline()
+    vmap = {}
+    entitymap = {}
+    while l != "" :
+      w = l.split()
+      if w[0] == "$MeshFormat":
+        l = fin.readline().split()
+        if float(l[0]) == 3.:
+          self.useFormat3 = True
+        elif int(float(l[0])) == 2 :
+          self.useFormat3 = False
+        else :
+          print("error : cannot read mesh format " + l[0])
+        l = fin.readline()
+      elif w[0] == "$PhysicalNames" :
+        n = int(fin.readline())
+        for i in range(n) :
+          dim, tag, name = fin.readline().split()
+          self.physicals[int(dim)][name[1:-1]] = int(tag)
+        fin.readline()
+      elif w[0] == "$Entities" and self.useFormat3:
+        n = int(fin.readline())
+        for i in range(n) :
+          l = fin.readline().split()
+          j, dim, nphys = int(l[0]), int(l[1]), int(l[2])
+          self.entities.append(mesh.entity(j, dim, [int(ip) for ip in l[3:3+nphys]]))
+          entitymap[(dim, j)] = self.entities[-1]
+        fin.readline()
+      elif w[0] == "$Nodes" :
+        n = int(fin.readline())
+        for i in range(n) :
+          if self.useFormat3 :
+            (j, x, y, z, t) = fin.readline().split()
+          else :
+            (j, x, y, z) = fin.readline().split()
+          self.vertices.append([float(x), float(y), float(z), int(j)])
+          vmap[int(j)] = self.vertices[-1]
+      elif w[0] == "$Elements" :
+        n = int(fin.readline())
+        for i in range(n) :
+          l = fin.readline().split()
+          if self.useFormat3 :
+            j, t, e, nf = int(l[0]), int(l[1]), int(l[2]), int(l[3])
+            nv = gmshType.Type[t].numVertices
+            vertices = [vmap[int(i)] for i in l[4:4+nv]]
+            partition = [int(i) for i in l[5 + nv : 5 + nv + int(l[4 + nv])]] if nf > nv else []
+          else :
+            j, t, nf, p, e = int(l[0]), int(l[1]), int(l[2]), int(l[3]), int(l[4])
+            vertices = [vmap[int(i)] for i in l[3 + nf:]]
+            partition = [int(i) for i in l[6 : 6 + int(l[5])]] if nf > 2 else []
+          edim = gmshType.Type[t].baseType.dimension
+          entity = entitymap.get((edim, e), None)
+          if not self.useFormat3 and not entity:
+            entity = mesh.entity(e, edim, [p])
+            self.entities.append(entity)
+            entitymap[(edim, e)] = entity
+          entity.elements.append(mesh.element(gmshType.Type[t], vertices, partition, j))
+          self.maxeid = max(self.maxeid, j)
+      elif w[0] == "$NodeData" :
+        print("Reading the data (works only in 1D)")
+        string_tags = []
+        real_tags = []
+        integer_tags = []
+        for i in range (int(fin.readline())):
+            string_tags.append(fin.readline())
+        for i in range (int(fin.readline())):
+            real_tags.append(float(fin.readline()))
+        for i in range (int(fin.readline())):
+            integer_tags.append(int(fin.readline()))
+        for i in range(len(self.vertices)):
+            l = fin.readline().split()
+            self.data.append(float(l[1]))
+      l = fin.readline()
+
+      pass
+  
   def getPhysicalNumber(self, dim, name) :
     t = self.physicals[dim].get(name, None)
     if not t :
@@ -159,7 +239,7 @@ class mesh() :
   def newVertex(self, x, y, z) :
     self.vertices.append((x, y, z, len(self.vertices) + 1))
     return self.vertices[-1]
- 
+
   def newElement(self, t, partition, entity, vertices) :
     self.maxeid += 1
     entity.elements.append(mesh.element(t, vertices, partition, self.maxeid))
@@ -189,3 +269,53 @@ class mesh() :
               vbentity.elements.append(f)
             else :
               e.faces[i] = allf[fs]
+
+  def export_matplotlib_triangulation(self):
+    x = [v[0] for v in self.vertices]
+    y = [v[1] for v in self.vertices]
+    triangles = []
+    for entity in self.entities:
+      if entity.dimension == 2:
+        for e in entity.elements:
+          triangles.append([v[3] - 1 for v in e.vertices]) 
+          # -1 because gmsh starts indexing at 1
+    triangulation = tri.Triangulation(x, y, triangles)
+    return triangulation
+
+  def get_matplotlib_triangulation_edges(self):
+    edges = []
+    for entity in self.entities:
+      if entity.dimension == 1:
+        # -1 because gmsh numbering starts at 1
+        v_first = entity.elements[0].vertices[0]
+        vertices_of_entity = [[v_first[0], v_first[1]]]
+        for e in entity.elements:
+          vertices_of_entity.append([e.vertices[1][0], e.vertices[1][1]])
+        edges.append(vertices_of_entity)
+    return edges
+
+def read_data(filename):
+  fin = open(filename, "r");
+  while fin.readline().split()[0] != "$NodeData" :
+    pass
+  string_tags = []
+  real_tags = []
+  integer_tags = []
+  data = []
+  for i in range (int(fin.readline())):
+    string_tags.append(fin.readline())
+  for i in range (int(fin.readline())):
+    real_tags.append(float(fin.readline()))
+  for i in range (int(fin.readline())):
+    integer_tags.append(int(fin.readline()))
+  for i in range(integer_tags[-1]):
+    l = fin.readline().split()
+    if len(l) == 2:
+      data.append(float(l[1]))
+    else:
+      l.pop(0)
+      data.append([float(d) for d in l])
+  return data
+
+# vim:ts=2:sts=2:sw=2
+
---
+++ sources/bin/plot.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+
+# Import packages
+import argparse # parse options
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.colors as colo
+import matplotlib.pyplot as plt
+import mesh #gmsh
+import multiprocessing
+import numpy as np
+import pdb # debugger
+import random
+import os
+
+# Parse options
+parser = argparse.ArgumentParser()
+parser.add_argument('label', type=str, help='label used when saving pictures')
+parser.add_argument('-f', '--flow', action='store_true', help='include pressure and velocity')
+parser.add_argument('-m', '--mesh', action='store_true', help='draw triangulation')
+parser.add_argument('-p', '--parallel', action='store_true', help='run in parallel')
+parser.add_argument('-e', '--extension', type=str, default='pdf', help='extension of output files')
+parser.add_argument('-i', '--iteration', type=str, help='iteration number')
+parser.add_argument('-s', '--step', type=int, default=1, help='step for iterations')
+parser.add_argument('--input', type=str, default="output", help='directory of data files')
+parser.add_argument('--output', type=str, default="pictures", help='directory for figure files')
+args = parser.parse_args()
+
+# Extract boundary of domain
+initial_mesh = mesh.mesh(args.input + '/mesh.msh')
+edges = initial_mesh.get_matplotlib_triangulation_edges()
+initial_triangulation = initial_mesh.export_matplotlib_triangulation()
+
+# Calculate limits size of domain
+x = initial_triangulation.x
+y = initial_triangulation.y
+dx = max(x) - min(x)
+dy = max(y) - min(y)
+padding = 0.1 * min(dx, dy)
+xlim = (min(x) - padding, max(x) + padding)
+ylim = (min(y) - padding, max(y) + padding)
+c_orientation = "vertical" if (dy > dx) else "horizontal"
+
+# Define colormaps
+c1 = (0.5, 1, 0.5)
+c2 = (0.5, 0.5, 1)
+color_dict = { 'red':   ((0, c1[0], c1[0]), (1, c2[0], c2[0])),
+    'green': ((0, c1[1], c1[1]), (1, c2[1], c2[1])),
+    'blue':  ((0, c1[2], c1[2]), (1, c2[2], c2[2])) }
+plt.register_cmap(name='blue_green', data=color_dict)
+colormaps = {}
+colormaps['phi'] = 'blue_green'
+colormaps['mu'] = 'jet'
+colormaps['pressure'] = 'jet'
+
+# Set canvas and font sizes
+inch_size = 20
+font_size = inch_size/10 * 20
+matplotlib.rc('font', size = font_size)
+matplotlib.rc('font', family='serif')
+matplotlib.rc('text', usetex=False)
+
+if args.iteration:
+  iterations = [int(args.iteration)]
+else:
+  n_files = len([f for f in os.listdir(args.input + '/done')])
+  n_plots = int(n_files/args.step)
+  iterations = [args.step*i for i in range(n_plots)]
+
+# Create directories if they are missing
+fields = ['phi', 'mu']
+if args.flow: fields += ['pressure', 'velocity']
+for f in fields:
+  directory = args.output + '/' + f
+  if not os.path.isdir(directory):
+    os.makedirs(directory)
+
+# for iteration in iterations:
+def plot_iteration(iteration):
+  # Load mesh
+  print("Generating plots for iteration " + str(iteration) + ".")
+  file_mesh = args.input + '/mesh/mesh-' + str(iteration) + '.msh'
+  triangulation = mesh.mesh(file_mesh).export_matplotlib_triangulation()
+
+  # Load data files
+  data = {}
+  for f in fields:
+    data[f] = mesh.read_data(args.input + '/' + f + '/' + f + '-' + str(iteration) + '.msh')
+
+  # Define titles
+  titles = {}
+  titles['phi'] = r'Phase field ($\phi$)'
+  titles['mu'] = r'Chemical potential ($\mu$)'
+  titles['pressure'] = r'Pressure ($p$)'
+  titles['velocity'] = r'Velocity ($\mathbf u$)'
+
+  # Set figure size and padding
+  plt.figure(figsize=(inch_size, inch_size))
+  plt.xlim(min(x) - padding, max(x) + padding)
+  plt.ylim(min(y) - padding, max(y) + padding)
+
+  # Edges
+  for entity in edges:
+    vertices_x = [v[0] for v in entity]
+    vertices_y = [v[1] for v in entity]
+    plt.plot(vertices_x, vertices_y, 'gray')
+  plt.axis('scaled')
+  plt.axis('off')
+
+  # Mesh
+  if args.mesh:
+    plt.triplot(triangulation, lw=1, color = 'k')
+
+  # Interface
+  plt.tricontour(triangulation, data['phi'], levels = [0], colors = 'k')
+
+  for f in fields:
+    if f == 'phi':
+      tricontourf = plt.tricontourf(triangulation, data[f], 40, cmap = 'blue_green', extend='both')
+    elif f == 'mu':
+      tricontourf = plt.tricontourf(triangulation, data[f], 40, cmap = 'jet')
+    elif f == 'pressure':
+      tricontourf = plt.tricontourf(triangulation, data[f], 40, cmap = 'jet')
+    elif f == 'velocity':
+      vx = [data[f][i][0] for i in range(len(data[f]))]
+      vy = [data[f][i][1] for i in range(len(data[f]))]
+      abs_v  = [np.sqrt(vx[i]*vx[i] + vy[i]*vy[i]) for i in range(len(data[f]))]
+      tricontourf = plt.tricontourf(triangulation, abs_v, 40)
+    colorbar = plt.colorbar(tricontourf, orientation = c_orientation, fraction = 0.05, pad = 0.05)
+    for c in tricontourf.collections:
+      c.set_edgecolor("face") # Fix for white lines between levels
+    if f == 'Velocity':
+      quiver = plt.quiver(x, y, vx, vy)
+    plt.title(titles[f], y = 1.02)
+    output = args.output + '/' + f + '/' + args.label + '-' + f + '-' + str(iteration) + '.' + args.extension
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+    plt.savefig(output, bbox_inches='tight')
+    for c in tricontourf.collections:
+      c.remove()
+    colorbar.remove()
+  plt.close()
+
+if args.parallel:
+  pool = multiprocessing.Pool()
+  pool.map(plot_iteration, iterations)
+else:
+  for iteration in iterations:
+    plot_iteration(iteration)
+
+# vim:ts=2:sts=2:sw=2
