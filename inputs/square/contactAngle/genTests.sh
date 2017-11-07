a5ee2cb7d33ab9110f4fa40290c935f4d9d103d2
Urbain Vaes <urbain@vaes.uk>
2017-11-07 15:41:32 +0100

    [inputs/square/contactAngle] Fix bugs in configuration

--- inputs/square/contactAngle/genTests.sh
+++ inputs/square/contactAngle/genTests.sh
@@ -5,5 +5,7 @@ contact_angles="pi/3 5*pi/12 pi/2 7*pi/12 2*pi/3"
 for angle in ${contact_angles}; do
     escaped_angle=$(echo ${angle} | tr "*" "t" | tr "/" "o")
     problem_name="${escaped_angle}-config.h"
-    cat > ${problem_name} config.h <(echo "#define CONTACT_ANGLE ${angle}")
+    cat > ${problem_name} \
+        <(echo "#include xstr(HERE/config.common)") \
+        <(echo "#define CONTACT_ANGLE ${angle}")
 done
--- inputs/square/contactAngle/pio2-config.h
+++ inputs/square/contactAngle/pio2-config.h
@@ -1,11 +1,2 @@
-#define GEOMETRY GITROOT/sources/geometries/square/simple-square.geo
-#define VIEW GITROOT/sources/views/squareCH.geo
-#define DIMENSION 2
-
-#define LX 2
-#define LY 1
-#define ADAPT
-#define OD2
-
-#define SOLVER_CONF GITROOT/inputs/square/contactAngle/problem.pde
+#include xstr(HERE/config.common)
 #define CONTACT_ANGLE pi/2
