b452bbce8bacc219bcf61ae283a0e3e0de241e2c
Urbain Vaes <urbain@vaes.uk>
2017-11-07 15:25:06 +0100

    Add contact angle files

---
+++ inputs/square/contactAngle/pio3-config.h
@@ -0,0 +1,11 @@
+#define GEOMETRY GITROOT/sources/geometries/square/simple-square.geo
+#define VIEW GITROOT/sources/views/squareCH.geo
+#define DIMENSION 2
+
+#define LX 2
+#define LY 1
+#define ADAPT
+#define OD2
+
+#define SOLVER_CONF GITROOT/inputs/square/contactAngle/problem.pde
+#define CONTACT_ANGLE pi/3
