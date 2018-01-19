cfd7ae3bbaaaffdc17e8c7f799714d4eb8f995d7
Urbain Vaes <urbain@vaes.uk>
2018-01-19 12:40:04 +0100

    Update inputs

---
+++ inputs/square/coalescence/medium-config.h
@@ -0,0 +1,12 @@
+#include xstr(HERE/config.common)
+
+// Time adatpation
+#if SOLVER_TIME_ADAPTATION_METHOD == AYMARD
+#define SOLVER_TIME_ADAPTATION_TOL_MIN 1e-4
+#define SOLVER_TIME_ADAPTATION_TOL_MAX 2e-4
+#endif
+
+// Mesh adaptation
+#define SOLVER_CN 0.01
+#define SOLVER_HMIN 0.001
+#define SOLVER_HMAX 0.05
