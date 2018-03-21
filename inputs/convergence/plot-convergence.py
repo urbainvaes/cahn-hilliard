ba31f08f1cab33a5fff5f2d9fcaf5dac85fe2c01
Urbain Vaes <urbain@vaes.uk>
2018-03-21 08:43:38 +0000

    Add plot of numerical dissipation

--- inputs/convergence/plot-convergence.py
+++ inputs/convergence/plot-convergence.py
@@ -6,6 +6,7 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import re
 
 # Parse options
 parser = argparse.ArgumentParser()
@@ -66,6 +67,36 @@ else:
     convergence_label = 'Mesh size $(h/h^*)$'
 plt.xlabel(convergence_label)
 plt.title('$L^{\infty}(0,T;L^2(\Omega))$ error')
+plt.xlim(min(x)/pow(2., .25), max(x)*pow(2, .25))
 plt.legend(loc='upper left')
 plt.savefig('pictures/' + args.label + '-convergence.pdf', bbox_inches='tight')
 plt.close()
+
+if args.convergence == 'time':
+    n_points = 6
+    data = []
+    for i in range(n_points):
+        data_file = '../' + str(i) + '/output/thermodynamics.txt'
+        with open(data_file, 'r') as f:
+            keys_line = f.readline().strip()
+            keys = re.split("\s+", keys_line)
+        data_aux = np.loadtxt(data_file, skiprows=1)
+        data.append({keys[j]: data_aux[:, j] for j in range(len(keys))})
+
+    plt.figure()
+    ratio = 2
+    x = ratio ** np.arange(0, n_points)
+    y = [max(np.abs(dic['int_numerical_dissipation'])) for dic in data]
+    coefs = np.polyfit(np.log2(x), np.log2(y), 1)
+    plt.plot(x, y, 'k.', label='Numerical results')
+    plt.plot(x, 2 ** coefs[1] * x ** coefs[0], 'k-',
+             label='Linear approximation: $y = C\,x^{{{:.2f}}}$'.format(coefs[0]))
+    plt.xscale('log', basex=2)
+    plt.yscale('log', basey=2)
+    convergence_label = 'Time step ($\\Delta t/\\Delta t ^*$)'
+    plt.xlabel(convergence_label)
+    plt.title('Numerical dissipation')
+    plt.xlim(min(x)/pow(2., .25), max(x)*pow(2, .25))
+    plt.legend(loc='upper left')
+    plt.savefig('pictures/' + args.label + '-numerical_dissipation.pdf', bbox_inches='tight')
+    plt.close()
