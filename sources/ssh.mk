4842f46cddd0fcee76883bb5c38acd7c554bd915
Urbain Vaes <urbain@vaes.uk>
2019-01-23 21:45:48 +0100

    Use 'balbec' host by default

--- sources/ssh.mk
+++ sources/ssh.mk
@@ -3,9 +3,10 @@
 ###########################
 cx1 = uv113@login.cx1.hpc.imperial.ac.uk
 macomp = uv113@macomp001.ma.ic.ac.uk
+balbec = urbain@155.198.193.89
 
-remoteHost ?= $(macomp)
-remoteRoot ?= cahn-hilliard
+remoteHost ?= $(balbec)
+remoteRoot ?= ~/phd/code/cahn-hilliard
 
 ifeq ($(remoteHost), $(cx1))
 	initCommand = module load intel-suite/2016.3 mpi/intel-5.1 gcc/6.2.0 hdf5/1.8.14-parallel gmsh/2.7.0 freefem++/3.49
